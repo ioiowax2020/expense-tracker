534dc5d1067bea04d718f22f8c35b1a49a23a8f8
Kou <ioiowax2020.jp@gmail.com>
2021-06-03 00:40:37 +0900

    feat:modify app.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
