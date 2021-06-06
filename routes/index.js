31148a75a6432582395807bb27812feef31a8dd2
Kou <ioiowax2020.jp@gmail.com>
2021-06-06 21:59:06 +0900

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 const filters = require('./modules/filters')
+const users = require('./modules/users')
+
 
 router.use('/', home)
 router.use('/records', records)
 router.use('/filters', filters)
-
+router.use('/users', users)
 
 
 
